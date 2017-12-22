88c5f3b271e12981d7dfb76c785ecbb1570924cf
rifan m fauzi <rifan.refun@gmail.com>
2017-12-22 16:40:46 +0700

    update RegisterTest

--- tests/Auth/RegisterTest.php
+++ tests/Auth/RegisterTest.php
@@ -2,13 +2,16 @@
 
 use Cemal\Tests\TestCase;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 
 class RegisterTest extends TestCase
 {
     public function testIndex()
     {
+        $email = 'tester'.str_random(7).'@gmail.com';
+        
         $data = [
-            'email' => 'tester'.str_random(7).'@gmail.com',
+            'email' => $email,
             'password' => '123cemal',
             'password_confirmation' => '123cemal',
             'name' => 'John Doe',
@@ -27,6 +30,75 @@ class RegisterTest extends TestCase
 
         $this->assertEquals(
             201, $response->status
+        );
+    }
+
+    public function testIndexBadInput()
+    {
+        $data = [
+            'password' => '123cemal',
+            'password_confirmation' => '123cemaludin',
+            'phone' => '093839434',
+            'address' => 'Jalan Anomali no 90',
+        ];
+
+        $this->json('POST', '/v1/register', $data);
+
+        $response = $this->getJsonResponse();
+
+        $this->assertEquals(
+            400, $response->status
+        );
+
+        $this->assertEquals(
+            true, property_exists($response->data, 'name') 
+        );
+
+        $this->assertEquals(
+            true, property_exists($response->data, 'email') 
+        );
+
+        $this->assertEquals(
+            true, property_exists($response->data, 'password') 
+        );
+    }
+
+    /**
+     * @depends testIndex
+     */
+    public function testVerify()
+    {
+        // get unverified user
+        $user = DB::table('users')->where('verified', false)->first();
+
+        $this->json('GET', '/v1/register/'.$user->verification_code);
+
+        $response = $this->getJsonResponse();
+
+        $this->assertEquals(
+            200, $response->status
+        );
+
+        // fetch updated user data
+        $user = DB::table('users')->where('id', $user->id)->first();
+
+        // make sure user has been verified after the call
+        $this->assertEquals(
+            true, $user->verified
+        );
+    }
+
+    /**
+     * @depends testIndex
+     */
+    public function testVerifyNotFound()
+    {
+        $this->json('GET', '/v1/register/'.substr(md5(date('Y-m-d H:i:s')), 0, 20));
+
+        $response = $this->getJsonResponse();
+
+        $this->assertEquals(
+            404, $response->status
         );
     }
 }
